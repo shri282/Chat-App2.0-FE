0c656f54016cc3c893a5b9d7f06ab15733ea2947
Harishkumar33-code <harishkumar.thirupathi@softsuave.com>
2024-04-06 18:32:55 +0530

    project added

---
+++ chat-app-server/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port localhost port ${PORT}....`);
+});
+
