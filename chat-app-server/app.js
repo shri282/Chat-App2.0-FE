5256528f6f5ed9eeb3790c1222c9782865f7749d
shri <shrikadhir10@gmail.com>
2024-04-12 02:52:31 +0530

    pagenotfount

--- chat-app-server/app.js
+++ chat-app-server/app.js
@@ -8,11 +8,22 @@ const PORT = process.env.PORT || 5000;
 connectDB(process.env.MONGO_URI);
 const app = express();
 app.use(express.json());
-app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.use("/api/user", userRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`server started on port localhost port ${PORT}....`);
