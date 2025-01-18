f845c5a44edc625c27e96210684195632eb95b56
shri <shrikadhir10@gmail.com>
2025-01-18 21:51:37 +0530

    added env

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const BE_BASE_URL = process.env.BE_BASE_URL ?? 'http://localhost:2001';
+
 export default axios.create({
-    baseURL: 'http://localhost:2001'
+    baseURL: BE_BASE_URL
 });
